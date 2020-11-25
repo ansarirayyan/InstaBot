496e0a34fd65c654ba3a8004f5d9ab73136e2779
ansarirayyan <mohdhaadiansari@gmail.com>
2020-11-25 15:36:19 -0800

    geckodriver headless mode

--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
+opts = Options()
+opts.log.level = "trace"
+opts.headless = True # enable headless mode to not get errors on env w/o desktop
+driver = webdriver.Firefox(options=opts)
 
 # isolates the username from the rest of the message content and searches for user
 def isolateUserString(msg, searchCommand):
     print(msg); # DEBUG
     username = msg.replace(searchCommand, '')
     return (username) # should I put spaces between the thing and '('s or nah?
+
